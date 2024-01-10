9a86142bae079619deaf9452c638a97f7883bf50
albertrf147 <albert@netenders.com>
2024-01-10 11:43:18 +0100

    moved formatter to local path

--- lua/qmk-formatter/init.lua
+++ lua/qmk-formatter/init.lua
@@ -18,7 +18,7 @@ M.format = function()
 	local root = syntax_tree[1]:root()
 
 	local run_formatter = function(text)
-		local target_path = "/../../qmk-formatter"
+		local target_path = "~/qmk-formatter"
 		local command = string.format("%s -i %s", vim.fn.shellescape(target_path), vim.fn.shellescape(text))
 		local formatted = vim.fn.systemlist(command)
 		return formatted
