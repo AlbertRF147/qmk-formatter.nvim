dc69c5fa28861fc7cf38242a64d8f0e5424f8d20
albertrf147 <albert@netenders.com>
2024-10-30 21:38:11 +0100

    changes

--- lua/qmk-formatter/init.lua
+++ lua/qmk-formatter/init.lua
@@ -9,8 +9,7 @@ M.format = function()
     ]]
 
 	local language_tree = require("nvim-treesitter.parsers").get_parser()
-	local Query = require("vim.treesitter.query")
-	local ok, query = pcall(Query.new, language_tree:lang(), query_string)
+	local ok, query = pcall(vim.treesitter.query.parse, language_tree:lang(), query_string)
 	print(ok)
 	print(query)
 	if not ok then
